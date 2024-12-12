273211c8bcdf3e547e13254ea9bf51d958163fee
aissa-dev1 <aissadev1@gmail.com>
2024-12-12 18:49:43 +0100

    feat: create a new reusabale page content (as a card)

---
+++ src/lib/components/reusable/page-content.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let { children, class: className, ...rest }: HTMLAttributes<HTMLElement> = $props();
+</script>
+
+<section
+	class={cn(
+		'absolute left-1/2 top-8 inline-block w-full -translate-x-1/2 rounded-none bg-card p-8 text-card-foreground shadow-md dark:shadow-none sm:w-3/4 sm:rounded-2xl lg:w-1/2',
+		className
+	)}
+	{...rest}
+>
+	{@render children?.()}
+</section>
--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -1,10 +1,11 @@
 <script>
+	import PageContent from '$lib/components/reusable/page-content.svelte';
 	import SideBar from '$lib/components/reusable/side-bar.svelte';
 </script>
 
 <main>
 	<SideBar />
-	<section class="ml-[var(--sidebar-width)]">
+	<PageContent>
 		<p>Hello Notifications</p>
-	</section>
+	</PageContent>
 </main>
