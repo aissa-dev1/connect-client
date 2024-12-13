78f908c37cb5787d8dc614a9852df4f1cd6cef3b
aissa-dev1 <aissadev1@gmail.com>
2024-12-13 10:57:34 +0100

    feat: design placeholder chats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script>
+	import ChatCard from '$lib/components/chat/chat-card.svelte';
 	import PageContent from '$lib/components/reusable/page-content.svelte';
 	import SideBar from '$lib/components/reusable/side-bar.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
 </script>
 
 <main>
 	<SideBar />
 	<PageContent>
-		<p>Hello Home</p>
+		<div class="flex flex-col gap-2">
+			<h3 class="page__content__title">Chats</h3>
+			<Input type="text" placeholder="Search chats" />
+		</div>
+		<div class="mt-3 grid grid-cols-1 gap-3">
+			{#each new Array(10).fill(null) as _}
+				<ChatCard />
+			{/each}
+		</div>
 	</PageContent>
 </main>
