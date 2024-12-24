f4aa111d89b90f52afd933f04990305812c6ce7e
aissa-dev1 <aissadev1@gmail.com>
2024-12-24 17:37:30 +0100

    Update phone navbar by including logo and needed link buttons

--- src/lib/components/reusable/phone-nav-bar.svelte
+++ src/lib/components/reusable/phone-nav-bar.svelte
@@ -1,5 +1,19 @@
+<script lang="ts">
+	import LinkButtonHome from './link-button/link-button-home.svelte';
+	import LinkButtonNotifications from './link-button/link-button-notifications.svelte';
+	import LinkButtonSearch from './link-button/link-button-search.svelte';
+	import LinkButtonSettings from './link-button/link-button-settings.svelte';
+	import LogoText from './logo-text.svelte';
+</script>
+
 <div
-	class="fixed bottom-0 left-1/2 z-10 h-16 w-full -translate-x-1/2 bg-card p-4 text-card-foreground sm:w-[90%] sm:rounded md:hidden"
+	class="fixed bottom-0 left-1/2 z-10 flex h-16 w-full -translate-x-1/2 items-center justify-between bg-card p-4 text-card-foreground sm:w-[90%] sm:rounded md:hidden"
 >
-	<p>Hello world!</p>
+	<LogoText />
+	<div class="flex__gap__md flex">
+		<LinkButtonHome />
+		<LinkButtonSearch />
+		<LinkButtonNotifications />
+		<LinkButtonSettings />
+	</div>
 </div>
