02158eda92a3164b81a0af82769487ddbb0a5353
aissa-dev1 <aissadev1@gmail.com>
2024-12-24 17:36:51 +0100

    Create new settings page/route

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import PageContent from '$lib/components/reusable/page-content.svelte';
+	import PhoneNavBar from '$lib/components/reusable/phone-nav-bar.svelte';
+	import SideBar from '$lib/components/reusable/side-bar.svelte';
+</script>
+
+<main>
+	<SideBar />
+	<PhoneNavBar />
+	<PageContent>
+		<h3 class="page__content__title">Settings</h3>
+	</PageContent>
+</main>
